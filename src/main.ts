2b2d02a9ad29754f7b7a52cc2cc4929ed1d3f89b
valentin alix <valentin-alix.850@outlook.fr>
2023-01-18 16:20:22 +0100

    [fix] add good npm command to start server

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-}
-);
+app.get("/", (req, res) => {
+  res.send("Hello Woriiiiiiiiiidd2!");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-}
-);
-
-
+  console.log(`Example app listening at http://localhost:${port}`);
+});
