5e462ff8c9c01272c749e8c21af1cbce3a4c97fe
Valentin <valentin-alix.850@outlook.fr>
2023-02-18 13:47:31 +0100

    fix: test server close after tests

--- src/Main.ts
+++ src/Main.ts
@@ -2,12 +2,12 @@ import express from 'express'
 import { customerRouter } from './routes/CustomerRouter'
 import { productRouter } from './routes/ProductRouter'
 
-export const app = express()
+const app = express()
 const port = 3000
 
 app.use('/customers/', customerRouter)
 app.use('/products/', productRouter)
 
-app.listen(port, () => {
+export const server = app.listen(port, () => {
   console.log(`Webship api listening at http://localhost:${port}`)
 })
--- src/tests/TestsRoutes.ts
+++ src/tests/TestsRoutes.ts
@@ -1,16 +1,21 @@
 import request from "supertest"
-import { app } from "../Main"
+import { server } from "../Main"
+
 
 describe('CustomerRouteTest', () => {
   test('test get customers', async () => {
-    const customersResponse = await request(app).get('/customers/2/')
+    const customersResponse = await request(server).get('/customers/2/')
     expect(customersResponse.body.name).toBe("Mr. Glenn Harvey")
   })
 })
 
 describe('ProductRouteTest', () => {
   test('test get products', async () => {
-    const productResponse = await request(app).get('/products/5')
+    const productResponse = await request(server).get('/products/5')
     expect(productResponse.body.name).toBe('Maurice Macejkovic')
   })
+})
+
+afterAll(() => {
+  server.close()
 })
