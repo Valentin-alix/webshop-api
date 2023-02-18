338dc39a011b5bb7ef2b2c54b4d74a272bf293eb
Valentin <valentin-alix.850@outlook.fr>
2023-02-18 14:01:18 +0100

    fix: use node fetch instead of just fetch

--- src/services/CustomerService.ts
+++ src/services/CustomerService.ts
@@ -1,7 +1,8 @@
 import { Customer } from '../models/Customer'
+import fetch from "node-fetch"
 
 export class CustomerService {
-  static async getCustomer (customerId: number): Promise<Customer> {
+  static async getCustomer(customerId: number): Promise<Customer[] | unknown> {
     return await fetch(`https://615f5fb4f7254d0017068109.mockapi.io/api/v1/customers/${customerId}`)
       .then(async response => {
         if (response.ok) {
@@ -16,7 +17,7 @@ export class CustomerService {
       })
   }
 
-  static async getCustomers (): Promise<Customer[]> {
+  static async getCustomers(): Promise<Customer[] | unknown> {
     return await fetch('https://615f5fb4f7254d0017068109.mockapi.io/api/v1/customers').then(async response => await response.json())
   }
 }
--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,7 +1,8 @@
 import { Product } from '../models/Product'
+import fetch from "node-fetch"
 
 export class ProductService {
-  static async getProduct(productId: number): Promise<Product> {
+  static async getProduct(productId: number): Promise<Product | unknown> {
     return await fetch(`https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products/${productId}`)
       .then(async response => {
         if (response.ok) {
@@ -17,7 +18,7 @@ export class ProductService {
   }
 
   // Récupération de tous les produits
-  static async getProducts(): Promise<Product[]> {
+  static async getProducts(): Promise<Product[] | unknown> {
     return await fetch('https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products/').then(async response => await response.json())
   }
 }
